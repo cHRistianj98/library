132fc5d7dc4dcf7d9b691ea898dd916feabc58f7
Krystian Kania <KRYSTIAN.KANIA@partner.bmw.de>
2022-01-19 23:14:04 +0100

    Add cors mapping

--- src/main/java/pl/distributed/library/security/SpringConfig.java
+++ src/main/java/pl/distributed/library/security/SpringConfig.java
@@ -1,13 +1,14 @@
 package pl.distributed.library.security;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class SpringConfig extends WebSecurityConfigurerAdapter {
+public class SpringConfig implements WebMvcConfigurer {
+
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().cors().disable();
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
     }
 }
